b1d71127fe3e0a2ff960d7a8fee78e2b5d9f0ef7
ahmad-khatib0 <ahmadkhatib923@gmail.com>
2025-12-30 12:50:53 +0300

    add serverError comp

---
+++ packages/chaty-app/src/components/app/server-error.tsx
@@ -0,0 +1,36 @@
+import 'server-only'
+
+import Image from 'next/image'
+
+type Props = {
+  msg?: string
+  description?: string
+}
+
+export const ServerError = ({ msg, description }: Props) => {
+  const defaultMsg = 'Sorry we encountered some technical issues during you last operation'
+  const defaultDesc =
+    'Please try to refresh you browser tab, or contact the support team if the problem persists.'
+
+  return (
+    <section className='grid grid-cols-[1fr,55%] w-full px-4'>
+      <div className='flex flex-col items-start justify-center mt-12'>
+        <h1 style={{ color: '#0d0c22' }} className='text-4xl font-bold'>
+          Internal Server Error
+        </h1>
+        <p className='mt-12 text-xl'>{msg ?? defaultMsg}</p>
+        <p className='mt-10 text-xl'> {description ?? defaultDesc} </p>
+      </div>
+      <div className='relative w-full h-[400px]'>
+        <Image
+          src={'/internal-error.png'}
+          alt='describe an internal error'
+          sizes='100%'
+          fill
+          priority
+          style={{ objectFit: 'cover' }}
+        />
+      </div>
+    </section>
+  )
+}
