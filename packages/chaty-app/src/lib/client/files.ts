1bb36b601a18079b6140533508909cfca1dc057e
ahmad-khatib0 <ahmadkhatib923@gmail.com>
2026-05-15 18:03:22 +0300

    app: humanFileSize util

---
+++ packages/chaty-app/src/lib/client/files.ts
@@ -0,0 +1,15 @@
+/**
+ * Human readable file size
+ */
+export function humanFileSize(bytes: number) {
+  const units = ['B', 'KB', 'MB', 'GB']
+  let size = bytes
+  let unitIndex = 0
+
+  while (size >= 1024 && unitIndex < units.length) {
+    size /= 1024
+    unitIndex++
+  }
+
+  return `${size.toFixed(1)} ${units[unitIndex]}`
+}
